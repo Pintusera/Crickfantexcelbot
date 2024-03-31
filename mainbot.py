eedb36f216ae27157609ee389605161f1681012e
Pintusera <89038849+Pintusera@users.noreply.github.com>
2024-03-31 13:48:07 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -1,14 +1,24 @@
 
 
 import telebot
-
+import telegramcrickexcel
+import suprocces
 BOT_TOKEN = token
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-def handle_excel_request(message):
-  user_data = message.text.strip()  # Get user input (data for Excel)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Hi! I can provide Excel files of any Criket match from ESPNcricinfo website\n"
+                          "Enter the corresponding match link.")
+@bot.message_handler(func=lambda message: True)
+def handle_link(message):
+    link = message.text
+    
 
+def handle_excel_request(message):
+  user_data = message.text.strip() 
+  
 
 
 bot.polling()
