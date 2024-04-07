43df89f856ba31cd31fa0c38553ef884f134f9a0
Pintusera <89038849+Pintusera@users.noreply.github.com>
2024-04-07 17:31:42 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -24,15 +24,17 @@ def option_date():
 def start(message):
     bot.reply_to(message, "Hi! I can provide Excel files of any Criket match from ESPNcricinfo website\n"
                           "Enter the corresponding match link.")
-@bot.message_handler(func=lambda message: True)
-def handle_link(message):
-    provide_link = message.text
-    print(provide_link)
-    bot.send_message(message.chat.id, "wait we are preparing Excel file")
-    final(provide_link)
-    bot.send_document(message.chat.id, name, caption='Here is your Excel file!')
-    bot.send_message(message.chat.id, "done")
 
+@bot.message_handler(func=option_func)
+def options(message):
+    dt_list=option_date()
+    markup types. ReplyKeyboardMarkup(row_width=2)
+    dtbtn1 types. KeyboardButton(dt_list[0])
+    dtbtn2 = types. KeyboardButton(dt_list[1])
+    dtbtn3 = types.KeyboardButton(dt_list[2])
+    dtbtn4 = types.KeyboardButton(dt_list[3])
+    markup.add(dtbtnl, dtbtn2,dtbtn3,dtbtn4)
+    bot.send_message(message.chat.id, "Choose an option:", reply_markup=markup)
 
 def handle_excel_request(message):
   user_data = message.text.strip() 
