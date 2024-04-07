0359db78d56e43a49835f9d4b99d8b7b6e8a3a00
Pintusera <89038849+Pintusera@users.noreply.github.com>
2024-04-07 16:23:09 +0530

    Create mainbot.py

---
+++ mainbot.py
@@ -0,0 +1,31 @@
+import os
+import telebot 
+from Alive import alive
+
+
+alive()
+
+BOT_TOKEN =os.environ.get('token')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start' , 'Start'])
+def start(message):
+    bot.reply_to(message, "Hi! I can provide Excel files of any Criket match from ESPNcricinfo website\n"
+                          "Enter the corresponding match link.")
+@bot.message_handler(func=lambda message: True)
+def handle_link(message):
+    provide_link = message.text
+    print(provide_link)
+    bot.send_message(message.chat.id, "wait we are preparing Excel file")
+    final(provide_link)
+    bot.send_document(message.chat.id, name, caption='Here is your Excel file!')
+    bot.send_message(message.chat.id, "done")
+    
+def handle_excel_request(message):
+  user_data = message.text.strip() 
+  
+
+
+bot.polling()
